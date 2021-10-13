5310251879973b6d4d4adae1a00e230cb27fdada
Dick Kluis <dick@concise-advice.net>
2021-10-13 11:56:31 -0400

    Added a New Turn option and optimized code reducing duplicate code lines

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -17,6 +17,7 @@ public class PlayerController : MonoBehaviour
     private float movementY;
     bool isMoving;
     bool isShowingRange;
+    bool turnInProgress;
 
     public float speed = 1;
     public float range = 2;
@@ -26,37 +27,49 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         rangeCircle = GameObject.Find("Range");
         player = GameObject.Find("Bird Token Grey");
-        OriginalX = player.GetComponent<Transform>().position.x;
-        OriginalY = player.GetComponent<Transform>().position.y;
-        rangeCircle.transform.position = new Vector2(OriginalX, OriginalY);
-        rangeCircle.GetComponent<SpriteRenderer>().color = new Color(80/255f, 240/255f, 100/255f, 0f);
-        isShowingRange = false;
+        turnInProgress = false;
+        OnNewTurn();
+        RangeViewToggle(false);
+        isMoving = false;
     }
 
     void OnMove(InputValue movementValue)
     {
+        if (!turnInProgress) { return; }
+
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
-        rangeCircle.GetComponent<SpriteRenderer>().color = new Color(80 / 255f, 240 / 255f, 100 / 255f, 25/255f);
-        isShowingRange = true;
-        //print($"onMove X: {movementX}, Y: {movementY}");
-
+        RangeViewToggle(true);
+        
         if (!isMoving)
         {
             isMoving = true;
-            OriginalX = player.GetComponent<Transform>().position.x;
-            OriginalY = player.GetComponent<Transform>().position.y;
-            //print($"Not isMoving = onMove X Pos: {OriginalX}, Y Pos: {OriginalY}");
         }
+        
     }
 
     void OnRange()
     {
         //print("onRange Activated");
-        if (isShowingRange) { rangeCircle.GetComponent<SpriteRenderer>().color = new Color(80 / 255f, 240 / 255f, 100 / 255f, 0f); isShowingRange = false; }
-        else { rangeCircle.GetComponent<SpriteRenderer>().color = new Color(80 / 255f, 240 / 255f, 100 / 255f, 25/255f); isShowingRange = true; }
+        if (isShowingRange) { RangeViewToggle(false); } else { RangeViewToggle(true); }
 
+    }
+
+    private void OnStop()
+    {
+        print("onStop Activated");
+        if (isMoving) { StopMoving(); }
+    }
+
+    private void OnNewTurn()
+    {
+        if (isMoving) { return;  }
+        //print("New Turn Activiated");
+        turnInProgress = true;
+        OriginalX = player.GetComponent<Transform>().position.x;
+        OriginalY = player.GetComponent<Transform>().position.y;
+        rangeCircle.transform.position = new Vector2(OriginalX, OriginalY);
     }
 
     void FixedUpdate()
@@ -67,25 +80,15 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        tfCurrent = player.GetComponent<Transform>();
-        //print($"Update Player X Pos: {tfCurrent.position.x}, Y Pos: {tfCurrent.position.y}");
-
         if (RangeExceeded())
         {
-            rb.velocity = Vector2.zero;
-            isMoving = false;
-            //print($"#################################################### Should stop moving#################################################");
-            print("Your Turn is Over");
-            OriginalX = player.GetComponent<Transform>().position.x;
-            OriginalY = player.GetComponent<Transform>().position.y;
-            rangeCircle.transform.position = new Vector2(OriginalX, OriginalY);
-            rangeCircle.GetComponent<SpriteRenderer>().color = new Color(80 / 255f, 240 / 255f, 100 / 255f, 0f);
-            isShowingRange = false;
+            if (isMoving) { StopMoving(); }
         }
     }
 
     private bool RangeExceeded()
     {
+        tfCurrent = player.GetComponent<Transform>();
         float deltaX = Math.Abs(tfCurrent.position.x - OriginalX);
         float deltaY = Math.Abs(tfCurrent.position.y - OriginalY);
 
@@ -95,5 +98,18 @@ public class PlayerController : MonoBehaviour
         if (deltaX >= range) { return true; }
         if (deltaY >= range) { return true; }
         return false;
+    }
+
+    private void StopMoving()
+    {
+        rb.velocity = Vector2.zero;
+        isMoving = false;
+        //print("Stopped Moving");
+    }
+
+    private void RangeViewToggle(bool TurnOn = false)
+    {
+        if (TurnOn) { rangeCircle.GetComponent<SpriteRenderer>().color = new Color(80 / 255f, 240 / 255f, 100 / 255f, 25 / 255f); isShowingRange = true; }
+        else { rangeCircle.GetComponent<SpriteRenderer>().color = new Color(80 / 255f, 240 / 255f, 100 / 255f, 0f); isShowingRange = false; }
     }
 }
