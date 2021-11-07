41be3d4e17791da1f57214b5842c634e6af24d90
Dick Kluis <dick@concise-advice.net>
2021-11-07 13:56:52 -0500

    Added the Quit (does not exit though)

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,5 +16,6 @@ public class MainMenuController : MonoBehaviour
  
     public void exitGame() {
         Application.Quit();
+        print("Should be quiting the game now");
     }
 }
